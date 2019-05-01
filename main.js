a29c9a4903542f5e65f780b135ff3d9c631a5ad8
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-01 12:05:50 -0400

    last push before lunch

---
+++ main.js
@@ -0,0 +1,20 @@
+//SCOPES
+
+//LET
+//VAR
+//CONST
+//NO KEYWORDS -- global variable
+
+let str = "Welcome to class!"
+
+function useless() {
+  console.log(str)
+  let str2 = 'woof'
+}
+
+function speak(){
+  console.log(str)
+}
+
+useless()
+speak()
